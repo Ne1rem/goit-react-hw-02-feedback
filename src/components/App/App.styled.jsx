0677d7d91717db253c1081e2599ed82745d3c471
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-08-12 20:09:40 +0300

    g

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 400px;
+  border-radius: 8px;
+  padding: 12px;
+  margin: 0 auto;
+  text-align: center;
+  background-color: #8181f1;
+  `;
---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+
+export const NoFeedback = styled.p`
+    font-size: 18px;
+    font-weight: 500;
+`;
